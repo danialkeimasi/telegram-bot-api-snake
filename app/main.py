6d693c27627035e4bb041828365ae86c453405c3
danial keimasi <danialkeimasi@gmail.com>
2019-07-31 23:53:21 +0430

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ updater = Updater(config.bot.token)
 dp = updater.dispatcher
 
 
-dp.add_handler(CommandHandler('hello', hello))
 def hello(bot, update):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
+
+dp.add_handler(CommandHandler('hello', hello))
