72e82c3b829444c0fef10297d607646025429c3e
danial keimasi <danialkeimasi@gmail.com>
2019-08-01 00:23:00 +0430

    debugging

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ dp = updater.dispatcher
 def hello(bot, update):
     print(update)
     
-    bot.send_message(update.message.from_user.chat_id, 'you said {}'.format(update.message.text))
+    bot.send_message(update.message.from_user.id, 'you said {}'.format(update.message.text))
 
 
 
