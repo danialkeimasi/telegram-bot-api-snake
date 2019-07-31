676044b4d0b13fa0a589149f968e70634ef6b6e5
danial keimasi <danialkeimasi@gmail.com>
2019-08-01 02:29:48 +0430

    debugging

--- bot.py
+++ bot.py
@@ -1,33 +1,26 @@
 import os
 import sys
 
+from tkinter import Tk
 from app.main import updater
 
 updater.start_polling()
 updater.idle()
 
 
-import os
-import sys
-from tkinter import Tk
- 
 CHECK_INTERVAL = 2000
- 
+
 last_mtime = os.path.getmtime(__file__)
 
 root = Tk()
- 
+
 def restart_if_changed():
     if os.path.getmtime(__file__) > last_mtime:
         print('file has changed => restarting')
- 
+
         os.execv(sys.executable, ['python'] + sys.argv)
- 
-    # Reschedule the checking.
+
     root.after(CHECK_INTERVAL, restart_if_changed)
- 
-# Schedule the checking.
+
 root.after(CHECK_INTERVAL, restart_if_changed)
- 
-# Enter the main processing loop.
 root.mainloop()
