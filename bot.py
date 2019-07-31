10fbb9d0598f36872326bc722d98e886db95dcd0
danial keimasi <danialkeimasi@gmail.com>
2019-08-01 02:34:59 +0430

    debugging

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ updater.idle()
 
 def run():
     print('restarting')
-    os.execv(sys.executable, ['python3'] + sys.argv)
+    os.execv(sys.executable, ['python3.6'] + sys.argv)
 
 
 run_process('./', run)
