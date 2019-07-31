d324cc770c3ed047a7003ff464dcca3dbe20485f
danial keimasi <danialkeimasi@gmail.com>
2019-07-31 23:49:38 +0430

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from telegram.ext import Updater, CommandHandler
 
 logger.setLevel(logging.INFO)
 updater = Updater(config.bot.token)
+dp = updater.dispatcher
 
 
+dp.add_handler(CommandHandler('hello', hello))
 def hello(bot, update):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
