6f23de72fae4f9928ea04b1b1ec85bf3cb13742a
danial keimasi <danialkeimasi@gmail.com>
2019-07-31 23:34:26 +0430

    change import places

--- snake/__init__.py
+++ snake/__init__.py
@@ -1,1 +1,8 @@
 from .page import *
+from .logger import logger
+import telegram
+import yaml
+import attrdict
+
+
+config = attrdict.AttrDict(yaml.safe_load(open("./config.yml", 'r')))
---
+++ snake/logger.py
@@ -0,0 +1,18 @@
+import logging
+import colorlog
+
+
+stream_handler = colorlog.StreamHandler()
+stream_handler.setFormatter(colorlog.ColoredFormatter())
+
+logging.basicConfig(
+    datefmt='%y-%b-%d %H:%M:%S',
+    format='%(levelname)8s:[%(asctime)s][%(filename)20s:%(lineno)4s -%(funcName)20s() ]: %(message)s',
+
+    level=logging.DEBUG,
+    handlers=[
+        stream_handler,
+    ]
+)
+
+logger = logging.getLogger('bot')
