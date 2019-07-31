35706a0e2675c4bafffa54b58fc312026fd73661
danial keimasi <danialkeimasi@gmail.com>
2019-07-31 23:59:35 +0430

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 
+# you can read this docs for writing bot
+# handler refrence : https://python-telegram-bot.readthedocs.io/en/stable/telegram.ext.html
+
 from snake import config, logger, logging
-from telegram.ext import Updater, CommandHandler
+from telegram import ext
 
 logger.setLevel(logging.INFO)
-updater = Updater(config.bot.token)
+updater = ext.Updater(config.bot.token)
 dp = updater.dispatcher
 
 
@@ -11,4 +14,4 @@ def hello(bot, update):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
 
-dp.add_handler(CommandHandler('hello', hello))
+dp.add_handler(ext.CommandHandler('hello', hello))
