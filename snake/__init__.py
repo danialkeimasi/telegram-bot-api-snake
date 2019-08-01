be6e06d0d0ab7ad2189cd043b95a1cd091cad998
danial keimasi <danialkeimasi@gmail.com>
2019-08-01 19:10:27 +0430

    debugging

--- snake/__init__.py
+++ snake/__init__.py
@@ -1,6 +1,9 @@
 from .page import *
 from .logger import logger, logging
 from .decorators import send_action, admin_only
-from .telebot import updater, config
 from telegram import ext, ChatAction, KeyboardButton, InlineKeyboardButton
 
+
+import yaml
+import attrdict
+config = attrdict.AttrDict(yaml.safe_load(open("./config.yml", 'r')))
