d415fa1144f190ce464b0702d488722dbddc4ac1
danial keimasi <danialkeimasi@gmail.com>
2019-08-01 18:50:25 +0430

    debugging

--- snake/page.py
+++ snake/page.py
@@ -1,5 +1,6 @@
 import telegram
-from telegram import KeyboardButton, InlineKeyboardButton
+from telegram import KeyboardButton, InlineKeyboardButton, ReplyKeyboardRemove
+
 
 def build_menu(buttons, n_cols, header_buttons, footer_buttons):
 
@@ -24,10 +25,20 @@ class Page:
         self.footer_buttons = footer_buttons
         self.n_cols = n_cols
 
+
     @property
     def markup(self):
+        if self.botton_list + self.header_buttons + self.footer_buttons == []:
+            return None
+
         if all([isinstance(i, KeyboardButton) for i in self.botton_list + self.header_buttons + self.footer_buttons]):
             return telegram.ReplyKeyboardMarkup(build_menu(self.botton_list, self.n_cols, self.header_buttons, self.footer_buttons))
-        else:
+        if all([isinstance(i, InlineKeyboardButton) for i in self.botton_list + self.header_buttons + self.footer_buttons]):
             return telegram.InlineKeyboardMarkup(build_menu(self.botton_list, self.n_cols, self.header_buttons, self.footer_buttons))
 
+        return None
+
+
+    @staticmethod
+    def remove_keyboard():
+        return ReplyKeyboardRemove()
