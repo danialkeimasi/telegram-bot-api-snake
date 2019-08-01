2c1854509826af95f983846fa556ee1d78d613ba
danial keimasi <danialkeimasi@gmail.com>
2019-08-01 18:26:56 +0430

    debugging

--- app/pages.py
+++ app/pages.py
@@ -1,3 +1,3 @@
 from snake import Page, InlineKeyboardButton
 
-main_page = Page('hi this is main page', page_type='inline', botton_list=[InlineKeyboardButton('ok')])
+main_page = Page('hi this is main page', page_type='inline', botton_list=[InlineKeyboardButton('ok', callback_data='kir')])
--- app/views.py
+++ app/views.py
@@ -7,5 +7,4 @@ from .pages import main_page
 
 @send_action(ChatAction.TYPING)
 def hello(bot, update):
-    print(main_page.markup)
-    bot.send_message(update.message.from_user.id, main_page.text, )
+    bot.send_message(update.message.from_user.id, main_page.text, reply_markup=main_page.markup)
