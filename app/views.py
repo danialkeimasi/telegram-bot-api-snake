116d301ed975a6a3b852f56c4266fcb7d4500da7
Danial Keimasi <danialkeimasi@gmail.com>
2019-08-17 22:03:23 +0430

    update view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from snake import send_action, ChatAction, is_authenticated
+from snake.decorators import debug
 from . import pages
 
-
+@debug
 @send_action(ChatAction.TYPING)
 def hello(bot, update):
     test = is_authenticated(bot, '@danidev', update.message.from_user.id)
-    bot.send_message(update.message.from_user.id, 'goh', reply_markup=pages.main_page.markup)
+    bot.send_message(update.message.from_user.id, **pages.main_page.send_message)
