6a80803bfd963a82864d3dd79ac5a75c117329e8
Danial Keimasi <danialkeimasi@gmail.com>
2019-09-02 19:14:16 +0430

    update the project maker

--- telekey/__main__.py
+++ telekey/__main__.py
@@ -1,6 +1,8 @@
 import os
 import click
 
+from distutils.dir_util import copy_tree
+
 
 @click.group()
 def cli():
@@ -10,9 +12,14 @@ def cli():
 @cli.command()
 @click.argument('botname', type=str)
 def newbot(botname):
-    print(os.getcwd())
-    pass
-
+    project_dir = os.path.join(os.getcwd(), botname)
+    if not os.path.isdir(project_dir):
+        copy_tree(
+            os.path.join(os.path.abspath(os.path.dirname(__file__)), 'project'),
+            os.path.join(os.getcwd(), botname)
+        )
+    else:
+        print(f'the {botname} directory is exist!')
 
 def main():
     cli()
