d0a82b46c9c0fd24b48983f717472200962955ce
Danial Keimasi <danialkeimasi@gmail.com>
2019-09-02 17:46:03 +0430

    add example remove token

--- example/message-tracker-bot/bot.py
+++ example/message-tracker-bot/bot.py
@@ -7,7 +7,7 @@ from app.hanlders import HANDLERS
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-updater = ext.Updater("869742149:AAFceL3S-AlQQwR7FhrD5rfe2oN8NsX-o34")
+updater = ext.Updater("YOUR TOKEN")
 
 for handler in HANDLERS:
     updater.dispatcher.add_handler(handler)
